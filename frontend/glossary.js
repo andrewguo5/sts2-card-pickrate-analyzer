9fa104954cb2092574baffed2d6134020a3ff6dc
Andrew Guo <andrew.guo5@gmail.com>
2026-04-15 17:16:15 -0400

    Whitespace changes

--- frontend/glossary.js
+++ frontend/glossary.js
@@ -8,33 +8,33 @@ window.Glossary = {
     "pick_rate": {
         title: "Pick Rate",
         short: "How often this card is picked when offered",
-        full: `How often this card is picked when offered. \n
+        full: `How often this card is picked when offered. \
         (# of times picked) / (# of times offered). Excludes shops.`
     },
 
     "skip_rate": {
         title: "Skip Rate",
         short: "How often the Skip button is clicked when this card is offered",
-        full: `How often the Skip button is clicked when this card is offered. \n
-        A skip is when none of the cards offered are chosen. If a card is not \n
-        picked, it is either skipped or another card is picked instead. This stat \n
-        tracks how often the card is skipped as opposed to losing to a better card. \n
+        full: `How often the Skip button is clicked when this card is offered. \
+        A skip is when none of the cards offered are chosen. If a card is not \
+        picked, it is either skipped or another card is picked instead. This stat \
+        tracks how often the card is skipped as opposed to losing to a better card. \
         (# of times this card was offered and skipped) / (# of times this card was offered)`
     },
 
     "baseline_skip_rate": {
         title: "Baseline Skip Rate",
         short: "On average, how often card rewards are skipped on a given floor.",
-        full: `On average, how often card rewards are skipped on a given floor, \n
-        for a given character/mode/ascension/player. If cards are skipped at a lower \n
-        frequency than baseline, then they are picked more often than expected. \n
+        full: `On average, how often card rewards are skipped on a given floor, \
+        for a given character/mode/ascension/player. If cards are skipped at a lower \
+        frequency than baseline, then they are picked more often than expected. \
         (# of times all cards were skipped) / (# of times a card reward was offered)`
     },
 
     "win_rate": {
         title: "Win Rate",
         short: "How often a deck containing at least one copy of this card wins.",
-        full: `How often a deck containing at least one copy of this card wins.\n
+        full: `How often a deck containing at least one copy of this card wins.\
         (# of wins with this card in the deck) / (total # of runs with this card in the deck)`
 
     },
@@ -43,8 +43,8 @@ window.Glossary = {
     "kernel_smoothing": {
         title: "Kernel Smoothing",
         short: "Averages the raw stats from the neighboring +/- 2 floors (b=2)",
-        full: `Averages the raw stats from the neighboring +/- 2 floors (b=2). \n
-        Kernel smoothing uses a sliding window to smooth out sparse data. \n
+        full: `Averages the raw stats from the neighboring +/- 2 floors (b=2). \
+        Kernel smoothing uses a sliding window to smooth out sparse data. \
         The parameter b is the band radius that specifies the width of this window.`
     },
 
